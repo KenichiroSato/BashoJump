c8ce6d920967142ada6bbfd28e7f1651a275d308
Kenichiro Sato <satok0702@gmail.com>
2015-05-09 14:18:20 +0900

    set physics world

--- BashoJump/GameScene.swift
+++ BashoJump/GameScene.swift
@@ -9,23 +9,30 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var motionManager : CMMotionManager!
     
     override func didMoveToView(view: SKView) {
+        initPhysicsWorld()
+        initMotionManager()
+    }
+
+    func initPhysicsWorld() {
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
+        self.physicsWorld.contactDelegate = self
+    }
+    
+    func initMotionManager() {
         motionManager = CMMotionManager()
-        
         motionManager.accelerometerUpdateInterval = 0.1 // get every 0.1 second
-        
         let accelerometerHandler:CMAccelerometerHandler = {
             (data:CMAccelerometerData!, error:NSError!) -> Void in
             println("x:\(data.acceleration.x) y:\(data.acceleration.y) z:\(data.acceleration.z)")
         }
-        
         motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: accelerometerHandler)
-        
     }
+    
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
     }
